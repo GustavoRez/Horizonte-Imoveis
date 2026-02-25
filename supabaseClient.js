a1ec72ea630f1ed214e29cc3e920638e330c5534
Gustavo <gustavoplay3@hotmail.com>
2026-02-25 11:37:02 -0300

    securtity

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,6 @@
+require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
-const SUPABASE_URL = 'https://nevgqmhzosgsdkwdipur.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_ckc1-KF-xW7vfy22BoFFUA_ubhUh0o3';
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 module.exports = { supabase };
